f171e3e2a3ae728a3a4e5f81bbe748ab4bab0216
Le Nam <ldnam806@gmail.com>
2023-02-07 00:02:08 +0700

    init source

---
+++ components/Home.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="page-wrapper">
+    <div class="container mx-auto px-4 md:px-0">
+      <div class="w-full md:w-[500px] mx-auto flex items-end gap-4 my-[20px]">
+        <div
+          class="my-avart w-[165px] h-[165px] rounded-[10px] overflow-hidden mx-auto shadow-inner"
+        >
+          <img
+            class="w-full h-full object-cover"
+            :src="require(`~/static/images/avatar.jpg`)"
+          />
+        </div>
+        <div class="flex-1">
+          <div class="font-semibold text-lg pl-[1px]">nam lee @boiz_alime</div>
+          <div class="flex items-center gap-1 my-3 text-gray-200">
+            <ion-icon name="location-sharp"></ion-icon>
+            <span class="text-[12px] font-bold">hochiminh city</span>
+          </div>
+
+          <div class="flex items-center gap-3">
+            <ion-icon class="text-[22px]" name="mail-outline"></ion-icon>
+            <ion-icon class="text-[22px]" name="logo-facebook"></ion-icon>
+            <ion-icon class="text-[22px]" name="logo-youtube"></ion-icon>
+            <ion-icon class="text-[22px]" name="logo-tiktok"></ion-icon>
+            <ion-icon class="text-[22px]" name="logo-instagram"></ion-icon>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center flex-col gap-3">
+        <div class="w-full md:w-[500px] mx-auto">
+          <h1 class="text-[25px] font-bold">About me</h1>
+          <div>
+            Halo mng, mình là <span class="font-bold">N4m</span>, hiện tại thì
+            mình đang là một
+            <span class="font-bold">FullstackOverflow Developer</span> ở tp Hồ
+            Chí Minh, mỗi ngày sau 8 tiếng code dưỡng sinh thì sở thích của mình
+            là edit linh tinh, chơi game, nghe nhạc, xem phim, viết blog và nấu
+            ăn.
+          </div>
+
+          <h1 class="text-[25px] font-bold my-2">Contact me</h1>
+          <div class="flex flex-col gap-4">
+            <button
+              class="flex items-center w-full border rounded-[18px] px-4 py-2 hover:backdrop-blur-md transition-all"
+            >
+              <ion-icon class="text-[22px]" name="logo-youtube"></ion-icon>
+              <span class="flex-1 font-bold text-center">youtube</span>
+            </button>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/5x7xx/"
+              class="flex items-center w-full border rounded-[18px] px-4 py-2 hover:backdrop-blur-md transition-all"
+            >
+              <ion-icon class="text-[22px]" name="logo-facebook"></ion-icon>
+              <span class="flex-1 font-bold text-center">facebook</span>
+            </a>
+            <button
+              class="flex items-center w-full border rounded-[18px] px-4 py-2 hover:backdrop-blur-md transition-all"
+            >
+              <ion-icon class="text-[22px]" name="mail-outline"></ion-icon>
+              <span class="flex-1 font-bold text-center">mail</span>
+            </button>
+          </div>
+
+          <!-- <div class="mt-4">
+            tóm tăt:
+            <ul>
+              <li>- được cái chơi game ngu nhưng hơi toxic.</li>
+              <li>- master chef món trứng rán và mì xào.</li>
+              <li>- hướng nội nhưng thích giao lưu.</li>
+              <li>- thích viết blog vl nhưng lười.</li>
+              <li>- hay sai chính tả.</li>
+              <li>- boiz alime.</li>
+            </ul>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+export default {
+  target: 'static',
+  head: {
+    title: 'Resume N4m',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: true,
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+      },
+    ],
+    script: [
+      {
+        type: 'module',
+        src: 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js',
+      },
+      {
+        src: 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js',
+      },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@/assets/styles/main', '@/assets/styles/common'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: ['@nuxt/postcss8', '@nuxtjs/eslint-module'],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+}
